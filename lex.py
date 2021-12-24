0ffcb624658e42c1793dcf9b1853c1b60155fa9c
Joe-Sin7h <josingh481@gmail.com>
2021-12-24 21:26:13 +0530

    Created Parser

--- lex.py
+++ lex.py
@@ -1,4 +1,4 @@
-from sly import Lexer
+from sly import Lexer, Parser
 
 class DogeLexer(Lexer):
     
@@ -6,7 +6,9 @@ class DogeLexer(Lexer):
     tokens = { 
                
         ID, NUMBER, PLUS, MINUS, TIMES,
-        DIVIDE, ASSIGN, LPAREN, RPAREN 
+        DIVIDE, ASSIGN, LPAREN, RPAREN,
+        # STRING, NAME
+
     }
     
     ignore = ' \t'
@@ -21,18 +23,88 @@ class DogeLexer(Lexer):
     ASSIGN  = r'='
     LPAREN  = r'\('
     RPAREN  = r'\)'
+    # STRING  = r'\".*?\"'
+    # NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
     def NUMBER(self, token):
         token.value = int(token.value)
-
         return token
+
+   
+
+class DogeParser(Parser):
+    tokens = DogeLexer.tokens
+
+    #Rules and grammar
+    # @_('')
+    # def statement(self, p):
+    #     pass
+
+    # @_('var_assign')
+    # def statement(self, p):
+    #     return p.var_assign
+
+    # @_('NAME "=" expr')
+    # def var_assign(self, p):
+    #     return ('var_assign', p.NAME, p.expr)
+
+    # @_('NAME "=" STRING')
+    # def var_assign(self,  p):
+    #     return ('var_assign', p.NAME, p.expr)
+        
+    @_('expr PLUS term')
+    def expr(self, p):
+        return p.expr + p.term
+
+    @_('expr MINUS term')
+    def expr(self, p):
+        return p.expr - p.term
+
+    @_('term')
+    def expr(self, p):
+        return p.term
+
+    @_('term TIMES factor')
+    def term(self, p):
+        return p.term * p.factor
+
+    @_('term DIVIDE factor')
+    def term(self, p):
+        return p.term/p.factor
+
+    @_('factor')
+    def term(self, p):
+        return p.factor
+
+    @_('NUMBER')
+    def factor(self, p):
+        return p.NUMBER
+
+    # @_('NAME')
+    # def expr(self, p):
+    #     return ('var', p.NAME)
+
+    @_('LPAREN expr RPAREN')
+    def factor(self, p):
+        return p.expr
+
+    
 
 
 if "__main__" == __name__:
+    # lexer = DogeLexer()
+    # data = "a = 1+2"
+    # for tokens in lexer.tokenize(data):
+    #     print(tokens)
+
     lexer = DogeLexer()
-    data = "a = 1+2"
-    for tokens in lexer.tokenize(data):
-        print(tokens)
+    parser = DogeParser()
+
+    while True:
+        try:
+            text = input('dogescript>> ')
+            result = parser.parse(lexer.tokenize(text))
+            print(result)
+        except EOFError:
+            break
     
-    
-        
